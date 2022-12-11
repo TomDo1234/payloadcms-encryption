99de082cf85e4097aad2bfe9ee15af1043bdfbce
TomDoFuture <108644869+TomDoFuture@users.noreply.github.com>
2022-12-11 16:25:43 +1100

    Declare module types

--- payloadcms-encryption.ts
+++ payloadcms-encryption.ts
@@ -3,7 +3,7 @@ import { FieldHook } from 'payload/types';
 const encryptKey: FieldHook = ({ req, value }) => (value ? req.payload.encrypt(value as string) : undefined);
 const decryptKey: FieldHook = ({ req, value }) => (value ? req.payload.decrypt(value as string) : undefined);
 
-const encryptField = {
+const encryptField : {[key:string] : [FieldHook]} = {
   beforeChange: [
     encryptKey,
   ],
